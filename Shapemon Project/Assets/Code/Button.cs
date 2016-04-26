bbb1b9c3cf24ea36ebe4bf843314d4f5b666e42f
cameronmathies <cmathies@mr.preble.com>
2016-04-26 13:10:42 -0400

    Main Menu Done

---
+++ Shapemon Project/Assets/Code/Button.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public void Play()
+    {
+        Application.LoadLevel("Level1");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
