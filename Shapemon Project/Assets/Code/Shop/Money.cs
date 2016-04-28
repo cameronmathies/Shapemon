a343b6f3fa481e36be825faa93cb6a735de95933
cameronmathies <cmathies@mr.preble.com>
2016-04-28 12:23:57 -0400

    Socre

---
+++ Shapemon Project/Assets/Code/Shop/Money.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Money : MonoBehaviour {
+
+    public int Cash;
+    public Text CashT;
+  
+    // Use this for initialization
+    void Start()
+    {
+        Cash = 0;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Test");
+        if (other.tag == "Item Level1")
+        {
+            Cash += 10;
+       }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        CashT.text = "GeoCash  " + Cash;
+
+        if (Cash > 1000000)
+        {
+            Cash -= 1000000;
+        }
+    }
+}
